34aa3a24e0c4242228bfb8a5f3657324351ca3cd
MD AMIN AZAD <41924091+Amin-Azad@users.noreply.github.com>
2025-08-08 03:14:45 +0200

    Update burger_patties_planner.py

--- burger_patties_planner.py
+++ burger_patties_planner.py
@@ -38,8 +38,8 @@ with st.form("planner"):
     st.divider()
     st.markdown("### 2) Current stock")
     c3, c4 = st.columns(2)
-    reg_in_stock = c3.number_input("Regular patties left in stock", min_value=0, step=1, value=0.0, format="%.0f)
-    mini_in_stock = c4.number_input("Mini patties left in stock", min_value=0, step=1, value=0.0, format="%.0f)
+    reg_in_stock = c3.number_input("Regular patties left in stock", min_value=0, step=1, value=2)
+    mini_in_stock = c4.number_input("Mini patties left in stock", min_value=0, step=1, value=4)
 
     submitted = st.form_submit_button("Calculate 💡", use_container_width=True)
 
